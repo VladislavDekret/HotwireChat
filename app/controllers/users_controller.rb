802767b36b6612ac9417fe6f32a7245ea0ef9589
vladyslav <dvlad20010629@gmail.com>
2022-05-24 14:44:34 +0300

    add user message in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by!(id: params[:id])
+
+    @user_messages = MessageDecorator.decorate_collection(
+      Message.includes(:likes)
+        .where(user_id: @user.id)
+        .where("messages.user_id" == "likes.user_id")
+        .where("messages.id" == "likes.message_id")
+    )
   end
 end
